fc5f7bceb475853ad3d64d1b5bf3e10f02c9ea7f
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-05-12 09:00:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+
+	Person.prototype.greet(){
+		console.log("Hello, my name is `${this.name}`, I am `${this.age}` years old. ")
+	}
+}
 
 function Employee(name, age, jobTitle) {}
 
